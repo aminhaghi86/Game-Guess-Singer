7c2e3fffdf9e3d3b91a22164638fe234e94f8d9a
amin <amin.haghi@hyperisland.se>
2022-10-25 23:14:53 +0200

    fix data files

--- data.js
+++ data.js
@@ -1,15 +1,17 @@
 export let singerDetails = [
   {
+    id: 1,
     img: [
       "./image/britneyspears.jpeg",
+      "./image/Roxette.jpg",
+      "./image/madonna.jpg",
       "./image/jenniferlopez.jpeg",
-      "./image/Nightwish.jpg",
-      "./image/Rammstein.jpg",
     ],
-    correct: 2,
-    audio: "./songs/GhostLoveScore.mp3",
+    correct: 1,
+    audio: "./songs/6.mp3",
   },
   {
+    id: 2,
     img: [
       "./image/celinedion.jpg",
       "./image/britneyspears.jpeg",
@@ -20,6 +22,7 @@ export let singerDetails = [
     audio: "./songs/celiondion.mp3",
   },
   {
+    id: 3,
     img: [
       "./image/katyperry.jpg",
       "./image/madonna.jpg",
@@ -30,6 +33,7 @@ export let singerDetails = [
     audio: "./songs/madonna.mp3",
   },
   {
+    id: 4,
     img: [
       "./image/britneyspears.jpeg",
       "./image/jenniferlopez.jpeg",
@@ -38,5 +42,94 @@ export let singerDetails = [
     ],
     correct: 3,
     audio: "./songs/AMERIKA.mp3",
+  },
+  {
+    id: 5,
+    img: [
+      "./image/circusofpowerband.jpg",
+      "./image/greatwhite.jpeg",
+      "./image/lagunsband.jpg",
+      "./image/gunaneoses.jpg",
+    ],
+    correct: 3,
+    audio: "./songs/1.mp3",
+  },
+  {
+    id: 6,
+    img: [
+      "./image/britneyspears.jpeg",
+      "./image/jenniferlopez.jpeg",
+      "./image/celinedion.jpg",
+      "./image/madonna.jpg",
+    ],
+    correct: 2,
+    audio: "./songs/2.mp3",
+  },
+  {
+    id: 7,
+    correct: 1,
+    img: [
+      "./image/britneyspears.jpeg",
+      "./image/sarahmclachlan.jpg",
+      "./image/madonna.jpg",
+      "./image/celinedion.jpg",
+    ],
+    audio: "./songs/3.mp3",
+  },
+  {
+    id: 8,
+    img: [
+      "./image/jenniferlopez.jpeg",
+      "./image/madonna.jpg",
+      "./image/britneyspears.jpeg",
+      "./image/Mariahcarey.jpg",
+    ],
+    correct: 3,
+    audio: "./songs/4.mp3",
+  },
+  {
+    id: 9,
+    img: [
+      "./image/britneyspears.jpeg",
+      "./image/jenniferlopez.jpeg",
+      "./image/Nightwish.jpg",
+      "./image/Rammstein.jpg",
+    ],
+    correct: 2,
+    audio: "./songs/GhostLoveScore.mp3",
+  },
+
+  {
+    id: 10,
+    img: [
+      "./image/britneyspears.jpeg",
+      "./image/jenniferlopez.jpeg",
+      "./image/madonna.jpg",
+      "./image/ToniBraxton.jpg",
+    ],
+    correct: 3,
+    audio: "./songs/8.mp3",
+  },
+  {
+    id: 11,
+    img: [
+      "./image/britneyspears.jpeg",
+      "./image/Genesis.jpg",
+      "./image/madonna.jpg",
+      "./image/Rammstein.jpg",
+    ],
+    correct: 1,
+    audio: "./songs/9.mp3",
+  },
+  {
+    id: 12,
+    img: [
+      "./image/britneyspears.jpeg",
+      "./image/jenniferlopez.jpeg",
+      "./image/ChrisIsaak.jpg",
+      "./image/Rammstein.jpg",
+    ],
+    correct: 2,
+    audio: "./songs/10.mp3",
   },
 ];
