2dab89fbfb5527677632f8d6bac8a893c27a4451
amin <amin.haghi@hyperisland.se>
2022-10-25 18:21:34 +0200

    add files - array-object

---
+++ data.js
@@ -0,0 +1,42 @@
+export let singerDetails = [
+  {
+    img: [
+      "./image/britneyspears.jpeg",
+      "./image/jenniferlopez.jpeg",
+      "./image/Nightwish.jpg",
+      "./image/Rammstein.jpg",
+    ],
+    correct: 2,
+    audio: "./songs/GhostLoveScore.mp3",
+  },
+  {
+    img: [
+      "./image/celinedion.jpg",
+      "./image/britneyspears.jpeg",
+      "./image/jenniferlopez.jpeg",
+      "./image/madonna.jpg",
+    ],
+    correct: 0,
+    audio: "./songs/celiondion.mp3",
+  },
+  {
+    img: [
+      "./image/katyperry.jpg",
+      "./image/madonna.jpg",
+      "./image/jenniferlopez.jpeg",
+      "./image/britneyspears.jpeg",
+    ],
+    correct: 1,
+    audio: "./songs/madonna.mp3",
+  },
+  {
+    img: [
+      "./image/britneyspears.jpeg",
+      "./image/jenniferlopez.jpeg",
+      "./image/madonna.jpg",
+      "./image/Rammstein.jpg",
+    ],
+    correct: 3,
+    audio: "./songs/AMERIKA.mp3",
+  },
+];
---
+++ script.js
@@ -0,0 +1,70 @@
+import { singerDetails } from "./data.js";
+let contents = document.getElementById("contents");
+var xindex = 0;
+let scorepoint = 0;
+let score = document.getElementById("score");
+let buttonStart = document.getElementById("btn");
+//
+const starGame = () => {
+  moveButton();
+  loadObjects(singerDetails);
+};
+const moveButton = () => {
+  buttonStart.style.transform = "translateY(-200%)";
+  buttonStart.innerHTML = "ENJOY!";
+};
+const loadObjects = (item) => {
+  var timer = 0;
+  console.log(item[xindex]);
+  item.forEach((el) => {
+    setTimeout(() => {
+      loadImages(el);
+      loadSongs(el);
+    }, timer);
+    timer = timer + 6500;
+  });
+};
+const loadImages = (image) => {
+  contents.innerHTML = `
+  <div class="image">
+     <img  class="targt" src="${image.img[0]}" alt="#">
+     <img  class="targt" src="${image.img[1]}" alt="#">
+     <img  class="targt" src="${image.img[2]}" alt="#">
+     <img  class="targt" src="${image.img[3]}" alt="#">
+   </div>
+  `;
+};
+const loadSongs = (song) => {
+  const audio = new Audio(song.audio);
+  audio.play();
+  chechAnswer(song, audio);
+  setTimeout(() => {
+    audio.pause();
+    audio.currentTime = 0;
+  }, 6500);
+};
+
+const chechAnswer = (el, audio) => {
+  let imageClick = document.querySelectorAll(".targt");
+  imageClick.forEach((element, ids) => {
+    element.addEventListener("click", () => {
+      stopTimer(audio);
+      if (ids === el.correct) {
+        scorepoint++;
+        element.style.border = "5px solid green";
+        score.innerHTML = `SCORE : ${scorepoint}`;
+      } else {
+        element.style.border = "5px dashed red";
+      }
+      setTimeout(() => {
+        element.parentNode.remove();
+      }, 200);
+    });
+  });
+};
+const stopTimer = (audio) => {
+  audio.pause();
+  audio.currentTime = 0;
+};
+
+buttonStart.addEventListener("click", starGame);
