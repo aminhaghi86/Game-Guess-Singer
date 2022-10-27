f159ea4db81c66191819709c7a37b9d5508d9dad
amin <amin.haghi@hyperisland.se>
2022-10-27 23:21:07 +0200

    solve timer part!!!!!!

--- script.js
+++ script.js
@@ -1,71 +1,49 @@
 import { singerDetails } from "./data.js";
 let contents = document.getElementById("contents");
-var xindex = 0;
 let scorepoint = 0;
 let score = document.getElementById("score");
 let buttonStart = document.getElementById("btn");
+let currentNumber = 0;
 //
-const starGame = () => {
-  moveButton();
-  loadObjects(singerDetails);
-};
-const moveButton = () => {
+const loadContent = () => {
   buttonStart.style.transform = "translateY(-200%)";
   buttonStart.innerHTML = "ENJOY!";
-};
-const loadObjects = (item) => {
-  var timer = 0;
-  item.forEach((el) => {
-    setTimeout(() => {
-      loadImages(el);
-      loadSongs(el);
-    }, timer);
-    timer = timer + 6500;
-  });
-};
-const loadImages = (image) => {
+  const item = singerDetails[currentNumber];
   contents.innerHTML = `
   <div class="image">
-     <img  class="targt" src="${image.img[0]}" alt="#">
-     <img  class="targt" src="${image.img[1]}" alt="#">
-     <img  class="targt" src="${image.img[2]}" alt="#">
-     <img  class="targt" src="${image.img[3]}" alt="#">
+     <img class="targt" src="${item.img[0]}" alt="#">
+     <img class="targt" src="${item.img[1]}" alt="#">
+     <img class="targt" src="${item.img[2]}" alt="#">
+     <img class="targt" src="${item.img[3]}" alt="#">
    </div>
   `;
-};
-const loadSongs = (song) => {
-  const audio = new Audio(song.audio);
+  const audio = new Audio(item.audio);
   audio.play();
-  checkAnswer(song, audio);
-  setTimeout(() => {
-    audio.pause();
-    audio.currentTime = 0;
-  }, 6500);
-};
-
-const checkAnswer = (el, audio) => {
-  let imageClick = document.querySelectorAll(".targt");
-  imageClick.forEach((element, ids) => {
-    element.addEventListener("click", () => {
-      stopTimer(audio);
-      if (ids === el.correct) {
-        scorepoint++;
-        element.style.border = "5px solid green";
-        score.innerHTML = `SCORE : ${scorepoint}`;
-      } else {
-        element.style.border = "5px dashed red";
-      }
-      element.parentNode.style.pointerEvents="none"
-      setTimeout(() => {
-        element.parentNode.remove();
-      }, 5000);
-      
+    document.querySelectorAll('.targt').forEach((el,i) => {
+        el.addEventListener('click',()=>{
+          if(i===item.correct){
+            setTimeout(() => {
+              score.style.color = 'green'
+            }, 100);
+            setTimeout(() => {
+              score.style.color = 'black'
+            }, 1000);
+            scorepoint++;
+            score.innerHTML = `SCORE : ${scorepoint}`
+          }else {
+            setTimeout(() => {
+              score.style.color = 'red'
+            }, 100);
+            setTimeout(() => {
+              score.style.color = 'black'
+            }, 1000);
+          }
+          currentNumber+=1;
+          audio.pause();
+          audio.currentTime=0;
+          loadContent()
+        })
     });
-  });
-};
-const stopTimer = (audio) => {
-  audio.pause();
-  audio.currentTime = 0;
-};
 
-buttonStart.addEventListener("click", starGame);
+}
+buttonStart.addEventListener("click", loadContent);
