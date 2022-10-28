b46b02ced39f7128a3309ab8c8e9fd75ae6a4369
amin <amin.haghi@hyperisland.se>
2022-10-28 13:46:28 +0200

    working on timer part .messy :))

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { singerDetails } from "./data.js";
-  let contents = document.getElementById("contents");
+let contents = document.getElementById("contents");
 let scorepoint = 0;
 let score = document.getElementById("score");
 let buttonStart = document.getElementById("btn");
@@ -10,36 +10,38 @@ const loadContent = () => {
   buttonStart.innerHTML = "ENJOY!";
   const item = singerDetails[currentNumber];
   contents.innerHTML = `
-  <div class="image">
-     <img class="targt" src="${item.img[0]}" alt="#">
-     <img class="targt" src="${item.img[1]}" alt="#">
-     <img class="targt" src="${item.img[2]}" alt="#">
-     <img class="targt" src="${item.img[3]}" alt="#">
-   </div>
-  `;
+    <div class="image">
+       <img class="targt" src="${item.img[0]}" alt="#">
+       <img class="targt" src="${item.img[1]}" alt="#">
+       <img class="targt" src="${item.img[2]}" alt="#">
+       <img class="targt" src="${item.img[3]}" alt="#">
+     </div>
+    `;
   const audio = new Audio(item.audio);
+  if (audio.current!==0){
+    audio.pause()
+    audio.currentTime=0;
 
   audio.play();
-    document.querySelectorAll('.targt').forEach((el,i) => {
-        el.addEventListener('click',()=>{
-          if(i===item.correct){
-        
-            scorepoint+=1;
-            score.innerHTML = `SCORE : ${scorepoint}`
-          }
-          audio.pause();
-          audio.currentTime=0
-          currentNumber+=1;
-     
-          loadContent()
-        })
-        setTimeout(function() {
-          audio.pause();
-          audio.currentTime=0
-          currentNumber += 1;
-          loadContent();
-      }, 6000);
+  document.querySelectorAll(".targt").forEach((el, i) => {
+    el.addEventListener("click", () => {
+      if (i === item.correct) {
+        scorepoint += 1;
+        score.innerHTML = `SCORE : ${scorepoint}`;
+      }
+      audio.pause();
+      audio.currentTime = 0;
+      currentNumber += 1;
+      loadContent();
+      console.log(singerDetails);
     });
-
+  });
+  setTimeout(() => {
+    audio.pause();
+    audio.currentTime = 0;
+    currentNumber += 1;
+    loadContent();
+  }, 6000);
 }
+};
 buttonStart.addEventListener("click", loadContent);
