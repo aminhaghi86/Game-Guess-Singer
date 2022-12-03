027e246dc374ffae1c7387c82331e9aceca017e4
amin <amin.haghi@hyperisland.se>
2022-12-03 12:23:11 +0100

    better structure of js file

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 import { singerDetails } from "./data.js";
 let contents = document.getElementById("contents");
-let scorepoint = 0;
 let score = document.getElementById("score");
 let buttonStart = document.getElementById("btn");
 let currentNumber = 0;
+let scorepoint = 0;
 //
-const loadContent = () => {
+const startFunction = async () => {
   buttonStart.style.transform = "translateY(-200%)";
   buttonStart.innerHTML = "ENJOY!";
   const item = singerDetails[currentNumber];
+  console.log(item);
   contents.innerHTML = `
     <div class="image">
        <img class="targt" src="${item.img[0]}" alt="#">
@@ -18,30 +19,31 @@ const loadContent = () => {
      </div>
     `;
   const audio = new Audio(item.audio);
-  if (audio.current!==0){
-    audio.pause()
-    audio.currentTime=0;
-
   audio.play();
-  document.querySelectorAll(".targt").forEach((el, i) => {
-    el.addEventListener("click", () => {
-      if (i === item.correct) {
-        scorepoint += 1;
-        score.innerHTML = `SCORE : ${scorepoint}`;
-      }
-      audio.pause();
-      audio.currentTime = 0;
-      currentNumber += 1;
-      loadContent();
-      console.log(singerDetails);
-    });
-  });
+   clickImage(item,audio);
   setTimeout(() => {
     audio.pause();
     audio.currentTime = 0;
     currentNumber += 1;
-    loadContent();
+  startFunction();
   }, 6000);
-}
 };
-buttonStart.addEventListener("click", loadContent);
+const clickImage = (item,audio) => {
+  let imagesTarget = document.querySelectorAll(".targt");
+console.log(imagesTarget);
+  imagesTarget.forEach((el, i) => {
+    el.addEventListener("click",async () => {
+      console.log("clicked");
+      audio.pause();
+      audio.currentTime=0;
+      if (i === item.correct) {
+        scorepoint += 1;
+        score.innerHTML = `SCORE : ${scorepoint}`;
+      }
+      currentNumber += 1;
+       startFunction();
+      console.log(singerDetails);
+    });
+  });
+};
+buttonStart.addEventListener("click", startFunction);
